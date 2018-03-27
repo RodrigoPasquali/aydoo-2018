fd675f54a88ed42ea30f1eaf95087c4373811ed0
RodrigoPasquali <r.pasquali2412@gmail.com>
2018-03-27 00:32:30 -0300

    Se crea clase SucesionFibo

--- fibo/src/main/java/ar/edu/untref/aydoo/Entrada.java
+++ fibo/src/main/java/ar/edu/untref/aydoo/Entrada.java
@@ -2,13 +2,19 @@ package ar.edu.untref.aydoo;
 
 public class Entrada {
 	
-	public int getNumero(String cadena) throws Exception {
-		try {
-   			int numeroObtenido = Integer.parseInt(cadena);
-   			return numeroObtenido;
-       	} catch (NumberFormatException nfe) {
-       		throw new Exception("Debe introducir el numero de suceciones");
-   		}
+	public int getNumero(String cadena[]) throws Exception {
+		String argumento;
+		int numeroObtenido = 0;
+		for (int i = 0; i < cadena.length; i++) {
+			argumento = cadena[i];
+			try {
+				numeroObtenido = Integer.parseInt(argumento);
+
+		       	} catch (NumberFormatException nfe) {
+		       		throw new Exception("Debe introducir el numero de suceciones");
+		   	}
+		}
+	    return numeroObtenido;
    }
 
 }
--- fibo/src/main/java/ar/edu/untref/aydoo/Program.java
+++ fibo/src/main/java/ar/edu/untref/aydoo/Program.java
@@ -1,23 +1,9 @@
 package ar.edu.untref.aydoo;
 
-import java.io.IOException;
-
 public class Program {
 	
-	public static final void main(String arg[]) throws IOException {
-		CalculadorFibo calculador = new CalculadorFibo();
-	/*	calculador.calcularSucecion(0);
-		calculador.calcularSucecion(1);
-		calculador.calcularSucecion(5);
-		calculador.calcularSucecion(8);
-	*/
-		
-		Formato formateador = new Formato();
-		
-		formateador.aplicarFormato(calculador.calcularSucecion(5));
-		System.out.println(formateador.getSucesion());
-		
-		formateador.aplicarFormato(calculador.calcularSucecion(8));
-		System.out.println(formateador.getSucesion());
+	public static final void main(String arg[]) throws Exception {
+		SucesionFibo sucesion = new SucesionFibo();
+		System.out.println(sucesion.generarRespuesta(arg));
 	}
 }
