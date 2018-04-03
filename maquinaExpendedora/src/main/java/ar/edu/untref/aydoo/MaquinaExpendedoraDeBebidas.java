1476d23465760aa84bcea9104deb5db0fac5a80a
Rodrigo Pasquali <rpasquali@teknos.local>
2018-04-03 16:32:43 -0300

    Se crea clase MaquinaExpendedoraDeBebidas y Vaso, funcionando correctamente el primer Assert del deberiaHacerCafeConLecheCon10DeAzucar()

---
+++ maquinaExpendedora/src/main/java/ar/edu/untref/aydoo/MaquinaExpendedoraDeBebidas.java
@@ -0,0 +1,9 @@
+package ar.edu.untref.aydoo;
+
+public class MaquinaExpendedoraDeBebidas {
+	
+	void hacerCafeConLecheConNDeAzucar(Vaso unVaso, int cantidadAzucar) {
+		unVaso.setCantidadDeAzucar(cantidadAzucar);
+	}
+
+}
---
+++ maquinaExpendedora/src/main/java/ar/edu/untref/aydoo/Vaso.java
@@ -0,0 +1,15 @@
+package ar.edu.untref.aydoo;
+
+public class Vaso {
+	
+	int cantidadDeAzucar;
+	
+	public void setCantidadDeAzucar(int cantidad){
+		this.cantidadDeAzucar = cantidad;
+	}
+
+	public int getCantidadDeAzucar() {
+		return this.cantidadDeAzucar;
+	}
+
+}
