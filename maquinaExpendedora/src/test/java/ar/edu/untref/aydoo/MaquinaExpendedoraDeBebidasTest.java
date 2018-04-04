acf7b950354b48675dcc346034d314005b997165
Rodrigo Pasquali <rpasquali@teknos.local>
2018-04-03 22:48:33 -0300

    Se agrega Assert tieneAzucar a deberiaHacerTeConLecheCon0DeAzucar() funcionando ok

--- maquinaExpendedora/src/test/java/ar/edu/untref/aydoo/MaquinaExpendedoraDeBebidasTest.java
+++ maquinaExpendedora/src/test/java/ar/edu/untref/aydoo/MaquinaExpendedoraDeBebidasTest.java
@@ -30,8 +30,8 @@ public class MaquinaExpendedoraDeBebidasTest {
 
 	        Assert.assertEquals(cantidadAzucar, unVaso.getCantidadDeAzucar());
 	        Assert.assertFalse(unVaso.tieneCafe());
-/*
 	        Assert.assertFalse(unVaso.tieneAzucar());
+/*
 	        Assert.assertTrue(unVaso.tieneTe());
 */
     }
