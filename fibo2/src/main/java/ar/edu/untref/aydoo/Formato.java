487cb7476e55b393b410cceaab2efb878f71ff75
Rodrigo Pasquali <rpasquali@teknos.local>
2018-04-15 23:33:36 -0300

    Se agrega test que devulve cadena en formato horizontal a TestFormato

--- fibo2/src/main/java/ar/edu/untref/aydoo/Formato.java
+++ fibo2/src/main/java/ar/edu/untref/aydoo/Formato.java
@@ -5,16 +5,29 @@ import java.util.List;
 public class Formato {
 	
 	String cadenaSucecion;
+	private String orientacion;
 		
-	public void aplicarFormato(List<Integer> listaNumeros){
-		cadenaSucecion = "fibo<" +listaNumeros.size() + ">: ";
-		for(int i = 0; i < listaNumeros.size(); i++){
-			cadenaSucecion = cadenaSucecion + listaNumeros.get(i).toString() + " ";
-		}
+	public Formato(String orientacionSeleccionada) {
+		this.orientacion = orientacionSeleccionada;
 	}
 
+	public void aplicarFormato(List<Integer> listaNumeros){
+		this.cadenaSucecion = "fibo<" +listaNumeros.size() + ">: ";
+		for(int i = 0; i < listaNumeros.size(); i++){
+			//cadenaSucecion = cadenaSucecion + listaNumeros.get(i).toString() + " ";
+			this.aplicarOrientacion(listaNumeros, i);
+		}
+	}
+	
+	private void aplicarOrientacion(List<Integer> listaNumeros, int posicion) {
+		if(this.orientacion.equals("v")) {
+		} else {
+			this.cadenaSucecion = this.cadenaSucecion + listaNumeros.get(posicion).toString() + " ";
+		}
+	}
+	
 	public String getSucesion(){
 		return cadenaSucecion;
-	}
+	} 
 
 }
