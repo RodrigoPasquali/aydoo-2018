4c30c3d8d3068a808910a3c1ef8e7ba7ac77add5
Rodrigo Pasquali <rpasquali@teknos.local>
2018-05-01 16:08:19 -0300

    En TestDescuento se agrega test que realiza descuento para clientes Registrados

--- libreria/src/test/java/ar/edu/untref/aydoo/TestDescuento.java
+++ libreria/src/test/java/ar/edu/untref/aydoo/TestDescuento.java
@@ -22,4 +22,22 @@ public class TestDescuento {
 	
 		assertEquals(valorEsperado, valorObtenido, 0.1);
 	}
+
+	@Test
+	public void aplicarDescuentoParaClienteRegistradoOtorgaUnDescuentoDe5PorCiento() {
+		Cliente laura = new Cliente("Laura", "Calle falsa 123");
+		Producto cuaderno = new Producto(100);
+		int anio = 2018;
+		int mes = 1;
+		int dia = 1;
+		Compra compraCuaderno = new Compra(cuaderno, anio, mes, dia);
+		laura.realizarComprar(compraCuaderno);
+		Descuento descuento = new DescuentoClienteRegistrado();
+		double valorEsperado = 95;
+		
+		double valorObtenido = descuento.aplicarDescuento(laura, anio, mes-1);
+	
+		assertEquals(valorEsperado, valorObtenido, 0.1);
+	}
 }
+
