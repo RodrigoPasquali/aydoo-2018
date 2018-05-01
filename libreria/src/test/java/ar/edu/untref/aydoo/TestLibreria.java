a9032b85d6f68ca665a6d7c8eb1fc64e6b3e911e
Rodrigo Pasquali <rpasquali@teknos.local>
2018-05-01 03:02:38 -0300

    Se agrega test de la compra de 2 productos de enero y uno en marzo devolviendo el monto a pagar de enero en TestLibreria

--- libreria/src/test/java/ar/edu/untref/aydoo/TestLibreria.java
+++ libreria/src/test/java/ar/edu/untref/aydoo/TestLibreria.java
@@ -42,4 +42,25 @@ public class TestLibreria {
 		assertEquals(valorEsperado, valorObtenido, 0.1);
 	}
 
+	@Test
+	public void elCobroDelMesDeberiaSerDe100CuandoSeCompraron2ProductosEnEnero2018yUnoEnMarzo() {
+		Libreria libreria = new Libreria();
+		Cliente juan = new Cliente("Juan","San juan 2890");
+		Producto libro = new Producto(50);
+		Producto lapicera = new Producto(50);
+		Producto goma = new Producto(10);
+		CuentaCorriente cuentaCorrienteJuan = new CuentaCorriente();
+		juan.setCuentaCorriente(cuentaCorrienteJuan);
+		Compra compraDeLapicera = new Compra(libro, 2018, 1, 2); 
+		Compra compraDeLibro = new Compra(lapicera, 2018, 1, 2);
+		Compra compraGoma = new Compra(goma, 2018, 3, 12);
+		juan.realizarComprar(compraDeLibro);
+		juan.realizarComprar(compraDeLapicera);
+		juan.realizarComprar(compraGoma);
+		double valorEsperado = 100;
+		
+		double valorObtenido = libreria.cobrarMontoMesCliente(juan, 2018, 1);
+		
+		assertEquals(valorEsperado, valorObtenido, 0.1);
+	}
 }
