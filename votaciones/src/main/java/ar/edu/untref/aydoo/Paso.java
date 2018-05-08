9832330a5cbfdcfdb841e1a1fcd88ebdf1c34515
Rodrigo Pasquali <rpasquali@teknos.local>
2018-05-08 00:27:36 -0300

    En TestPaso se agregan 2 test para calcular los ganadores en la provincia de bsas y cordoba

--- votaciones/src/main/java/ar/edu/untref/aydoo/Paso.java
+++ votaciones/src/main/java/ar/edu/untref/aydoo/Paso.java
@@ -17,10 +17,25 @@ public class Paso {
 		this.votos.add(voto);
 	}
 
-	public Candidato getCandidatoConMasVotosEnProvincia(Provincia bsas) {
+	public Candidato getCandidatoConMasVotosEnProvincia(Provincia provincia) {
 		Candidato candidatoGanador = this.votos.get(0).getCandidato();
-
+		this.contarVotosPorProvincia(provincia);
+		for(int i = 1; i < this.votos.size(); i++) {
+			Candidato candidatoActual = this.votos.get(i).getCandidato();
+			if(candidatoGanador.getCantidadVotos() < candidatoActual.getCantidadVotos()) {
+				candidatoGanador = candidatoActual;
+			}
+		}
 		return candidatoGanador;
+	}
+	
+	private void contarVotosPorProvincia(Provincia provincia) {
+		for(int i = 0; i < this.votos.size(); i++) {
+			Voto votoActual = this.votos.get(i);
+			if(provincia.equals(votoActual.getProvincia())) {
+				votoActual.getCandidato().sumarVoto();
+			}
+		}
 	}
 
 }
--- votaciones/src/test/java/ar/edu/untref/aydoo/TestPaso.java
+++ votaciones/src/test/java/ar/edu/untref/aydoo/TestPaso.java
@@ -22,4 +22,60 @@ public class TestPaso {
 		assertEquals(valorEsperado, valorObtenido);
 	}
 
+	@Test
+	public void elGanadorDeLaProvinciaDeBsAsDeberiaSerPepeCon3Votos() {
+		Paso paso = new Paso("2018");
+		Partido rojo = new Partido("Rojo");
+		Candidato juan = new Candidato("Juan");
+		Candidato pepe = new Candidato("Pepe");
+		Provincia bsas = Provincia.BUENOS_AIRES;
+		Provincia cordoba = Provincia.CORDOBA;
+		juan.afiliarAPartido(rojo);
+		Voto voto1 = new Voto(juan, bsas);
+		Voto voto2 = new Voto(juan, cordoba);	
+		Voto voto3 = new Voto(pepe, bsas);
+		Voto voto4 = new Voto(pepe, bsas);
+		Voto voto5 = new Voto(juan, cordoba);
+		
+		Candidato valorEsperado = pepe;
+		
+		paso.aniadirVoto(voto1);
+		paso.aniadirVoto(voto2);
+		paso.aniadirVoto(voto3);
+		paso.aniadirVoto(voto4);
+		paso.aniadirVoto(voto5);
+		
+		Candidato valorObtenido = paso.getCandidatoConMasVotosEnProvincia(bsas);
+		
+		assertEquals(valorEsperado, valorObtenido);
+	}
+	
+	@Test
+	public void elGanadorDeLaProvinciaDeCordobaDeberiaSerJuanCon3Votos() {
+		Paso paso = new Paso("2018");
+		Partido rojo = new Partido("Rojo");
+		Candidato juan = new Candidato("Juan");
+		Candidato pepe = new Candidato("Pepe");
+		Provincia bsas = Provincia.BUENOS_AIRES;
+		Provincia cordoba = Provincia.CORDOBA;
+		juan.afiliarAPartido(rojo);
+		Voto voto1 = new Voto(juan, bsas);
+		Voto voto2 = new Voto(juan, cordoba);	
+		Voto voto3 = new Voto(pepe, bsas);
+		Voto voto4 = new Voto(pepe, bsas);
+		Voto voto5 = new Voto(juan, cordoba);
+		
+		Candidato valorEsperado = juan;
+		
+		paso.aniadirVoto(voto1);
+		paso.aniadirVoto(voto2);
+		paso.aniadirVoto(voto3);
+		paso.aniadirVoto(voto4);
+		paso.aniadirVoto(voto5);
+		
+		Candidato valorObtenido = paso.getCandidatoConMasVotosEnProvincia(cordoba);
+		
+		assertEquals(valorEsperado, valorObtenido);
+	}
+
 }
