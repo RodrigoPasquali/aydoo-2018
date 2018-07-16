8720a4886598c165d30cfe21b457c7d952c659d0
Rodrigo Pasquali <rpasquali@teknos.local>
2018-07-15 23:52:51 -0300

    Se agrega excepcionCotizacionDolarIncorrecta y excepcionMontoMenorACotizacionDolar con test correspondientes

--- TpFinal/src/test/java/ar/edu/untref/aydoo/TestCompraDeDolares.java
+++ TpFinal/src/test/java/ar/edu/untref/aydoo/TestCompraDeDolares.java
@@ -3,6 +3,9 @@ package ar.edu.untref.aydoo;
 import org.junit.Assert;
 import org.junit.Test;
 
+import excepciones.ExcepcionCotizacionDolarIncorrecta;
+import excepciones.ExcepcionMontoMenorACotizacionDolar;
+
 public class TestCompraDeDolares {
 	@Test
 	public void realizarLaCompraDeDolaresCon1000PesosCotizacionInicial20CotizacionFinal28DeberiaDevolverGananciaDe400() {	  
@@ -12,5 +15,38 @@ public class TestCompraDeDolares {
 		double valorObtenido = comprarDolares.calcularGanancia(1000, 20, 28);
 		
 		Assert.assertEquals(valorEsperado, valorObtenido, 0.1);
+	}
+
+	@Test
+	public void realizarLaCompraDeDolaresCon10PesosCotizacionInicial20DeberiaLanzarExcepcionMontoMenorACotizacionDolar() {	  
+		CompraDeDolares comprarDolares = new CompraDeDolares();
+		
+		try {
+			comprarDolares.calcularGanancia(10, 20, 28);
+		}catch (ExcepcionMontoMenorACotizacionDolar e){
+
+	    }		
+	}
+	
+	@Test
+	public void realizarLaCompraDeDolaresConCotizacionInicial0DeberiaLanzarExcepcionCotizacionDolarIncorrecta() {	  
+		CompraDeDolares comprarDolares = new CompraDeDolares();
+		
+		try {
+			comprarDolares.calcularGanancia(10, 0, 28);
+		}catch (ExcepcionCotizacionDolarIncorrecta e){
+
+	    }		
+	}
+	
+	@Test
+	public void realizarLaCompraDeDolaresConCotizacionFinal0DeberiaLanzarExcepcionCotizacionDolarIncorrecta() {	  
+		CompraDeDolares comprarDolares = new CompraDeDolares();
+		
+		try {
+			comprarDolares.calcularGanancia(10, 0, 28);
+		}catch (ExcepcionCotizacionDolarIncorrecta e){
+
+	    }		
 	}
 }
