187a7a55da5baf4001439559603bacd623983540
Rodrigo Pasquali <rpasquali@teknos.local>
2018-07-16 01:08:56 -0300

    CompraDeDolares pasa a heredar de Inversion

--- TpFinal/src/main/java/ar/edu/untref/aydoo/CompraDeDolares.java
+++ TpFinal/src/main/java/ar/edu/untref/aydoo/CompraDeDolares.java
@@ -3,17 +3,27 @@ package ar.edu.untref.aydoo;
 import excepciones.ExcepcionCotizacionDolarIncorrecta;
 import excepciones.ExcepcionMontoMenorACotizacionDolar;
 
-public class CompraDeDolares {
-	public double calcularGanancia(double montoPesosInicial, double cotizacionInicial, double cotizacionFinal) {
+public class CompraDeDolares extends Inversion{	
+	private double montoPesosInicial;
+	private double cotizacionInicial;
+	private double cotizacionFinal;
+
+	public CompraDeDolares(double montoPesosInicialIngresado, double cotizacionInicialIngresado, double cotizacionFinalIngresado) {
+		this.montoPesosInicial = montoPesosInicialIngresado;
+		this.cotizacionInicial = cotizacionInicialIngresado;
+		this.cotizacionFinal = cotizacionFinalIngresado;
+	}
+	
+	public double calcularGanancia() {
 		double gananciaObtenida = 0;	
-		if(montoPesosInicial < cotizacionInicial) {
+		if(this.montoPesosInicial < this.cotizacionInicial) {
 			throw new ExcepcionMontoMenorACotizacionDolar();
 		}
-		if(cotizacionInicial <= 0 || cotizacionFinal <= 0) {
+		if(this.cotizacionInicial <= 0 || this.cotizacionFinal <= 0) {
 			throw new ExcepcionCotizacionDolarIncorrecta();
 		}
-		double montoDolares = montoPesosInicial / cotizacionInicial;
-		double montoPesosFinal = montoDolares * cotizacionFinal;
+		double montoDolares = this.montoPesosInicial / this.cotizacionInicial;
+		double montoPesosFinal = montoDolares * this.cotizacionFinal;
 		gananciaObtenida = montoPesosFinal - montoPesosInicial;
 		return gananciaObtenida;
 	}
--- TpFinal/src/test/java/ar/edu/untref/aydoo/TestCompraDeDolares.java
+++ TpFinal/src/test/java/ar/edu/untref/aydoo/TestCompraDeDolares.java
@@ -9,20 +9,20 @@ import excepciones.ExcepcionMontoMenorACotizacionDolar;
 public class TestCompraDeDolares {
 	@Test
 	public void realizarLaCompraDeDolaresCon1000PesosCotizacionInicial20CotizacionFinal28DeberiaDevolverGananciaDe400() {	  
-		CompraDeDolares comprarDolares = new CompraDeDolares();
+		CompraDeDolares comprarDolares = new CompraDeDolares(1000, 20, 28);
 		double valorEsperado = 400;
 		
-		double valorObtenido = comprarDolares.calcularGanancia(1000, 20, 28);
+		double valorObtenido = comprarDolares.calcularGanancia();
 		
 		Assert.assertEquals(valorEsperado, valorObtenido, 0.1);
 	}
 
 	@Test
 	public void realizarLaCompraDeDolaresCon10PesosCotizacionInicial20DeberiaLanzarExcepcionMontoMenorACotizacionDolar() {	  
-		CompraDeDolares comprarDolares = new CompraDeDolares();
+		CompraDeDolares comprarDolares = new CompraDeDolares(10, 20, 28);
 		
 		try {
-			comprarDolares.calcularGanancia(10, 20, 28);
+			comprarDolares.calcularGanancia();
 		}catch (ExcepcionMontoMenorACotizacionDolar e){
 
 	    }		
@@ -30,10 +30,10 @@ public class TestCompraDeDolares {
 	
 	@Test
 	public void realizarLaCompraDeDolaresConCotizacionInicial0DeberiaLanzarExcepcionCotizacionDolarIncorrecta() {	  
-		CompraDeDolares comprarDolares = new CompraDeDolares();
+		CompraDeDolares comprarDolares = new CompraDeDolares(10, 0, 28);
 		
 		try {
-			comprarDolares.calcularGanancia(10, 0, 28);
+			comprarDolares.calcularGanancia();
 		}catch (ExcepcionCotizacionDolarIncorrecta e){
 
 	    }		
@@ -41,10 +41,10 @@ public class TestCompraDeDolares {
 	
 	@Test
 	public void realizarLaCompraDeDolaresConCotizacionFinal0DeberiaLanzarExcepcionCotizacionDolarIncorrecta() {	  
-		CompraDeDolares comprarDolares = new CompraDeDolares();
+		CompraDeDolares comprarDolares = new CompraDeDolares(10, 0, 28);
 		
 		try {
-			comprarDolares.calcularGanancia(10, 0, 28);
+			comprarDolares.calcularGanancia();
 		}catch (ExcepcionCotizacionDolarIncorrecta e){
 
 	    }		
