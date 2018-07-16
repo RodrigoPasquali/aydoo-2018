a6ec937d34ead72b4977e0c118c6e97b17defd7e
Rodrigo Pasquali <rpasquali@teknos.local>
2018-07-15 21:47:05 -0300

    Se agregan test para plazo fijo precancelable para expeciones

--- TpFinal/src/test/java/ar/edu/untref/aydoo/TestPlazoFijoPrecancelable.java
+++ TpFinal/src/test/java/ar/edu/untref/aydoo/TestPlazoFijoPrecancelable.java
@@ -3,6 +3,10 @@ package ar.edu.untref.aydoo;
 import org.junit.Assert;
 import org.junit.Test;
 
+import excepciones.ExcepcionDiasIncorrectos;
+import excepciones.ExcepcionInteresIncorrecto;
+import excepciones.ExcepcionMontoIncorrecto;
+
 public class TestPlazoFijoPrecancelable {	
 	@Test
 	public void realizarUnPlazoFijoPrecancelableDeberiaDevolver12500DeGananciaCuandoElMontoEs5000YElInteresEs25() {	  
@@ -24,4 +28,69 @@ public class TestPlazoFijoPrecancelable {
 		Assert.assertEquals(valorEsperado, valorObtenido, 0.1);
 	}
 
+	@Test
+	public void realizarUnPlazoFijoPrecancelableDe10DiasInicialesDeberiaLanzarExcepcionDiasIncorrectos() {	  
+		PlazoFijoPrecancelable plazoFijoPrecancelable = new PlazoFijoPrecancelable();
+		
+		try {
+			plazoFijoPrecancelable.calcularGanancia(10, 6, 25, 50000);
+		}catch (ExcepcionDiasIncorrectos e){
+
+	    }
+	}
+	
+	@Test
+	public void realizarUnPlazoFijoPrecancelableDe29DiasInicialesDeberiaLanzarExcepcionDiasIncorrectos() {	  
+		PlazoFijoPrecancelable plazoFijoPrecancelable = new PlazoFijoPrecancelable();
+		
+		try {
+			plazoFijoPrecancelable.calcularGanancia(29, 6, 25, 50000);
+		}catch (ExcepcionDiasIncorrectos e){
+
+	    }
+	}
+
+	@Test
+	public void realizarUnPlazoFijoPrecancelableDeMonto0DeberiaLanzarExcepcionMontoIncorrecto() {	  
+		PlazoFijoPrecancelable plazoFijoPrecancelable = new PlazoFijoPrecancelable();
+		
+		try {
+			plazoFijoPrecancelable.calcularGanancia(100, 30, 25, 0);
+		}catch (ExcepcionMontoIncorrecto e){
+
+	    }
+	}
+	
+	@Test
+	public void realizarUnPlazoFijoPrecancelableDeMonto1NegativoDeberiaLanzarExcepcionMontoIncorrecto() {	  
+		PlazoFijoPrecancelable plazoFijoPrecancelable = new PlazoFijoPrecancelable();
+		
+		try {
+			plazoFijoPrecancelable.calcularGanancia(30, 15, 25, -1);
+		}catch (ExcepcionMontoIncorrecto e){
+
+	    }
+	}
+	
+	@Test
+	public void realizarUnPlazoFijoPrecancelableDeInteres0DeberiaLanzarExcepcionInteresIncorrecto() {	  
+		PlazoFijoPrecancelable plazoFijoPrecancelable = new PlazoFijoPrecancelable();
+		
+		try {
+			plazoFijoPrecancelable.calcularGanancia(100, 100, 0, 50000);
+		}catch (ExcepcionInteresIncorrecto e){
+
+	    }
+	}
+	
+	@Test
+	public void realizarUnPlazoFijoPrecancelableDeInteres1NegativoDeberiaLanzarExcepcionInteresIncorrecto() {	  
+		PlazoFijoPrecancelable plazoFijoPrecancelable = new PlazoFijoPrecancelable();
+		
+		try {
+			plazoFijoPrecancelable.calcularGanancia(100, 100, -1, 50000);
+		}catch (ExcepcionInteresIncorrecto e){
+
+	    }
+	}
 }
