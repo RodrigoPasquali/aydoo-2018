466136d1635294732a3df5d1c4056ff0ede255db
Rodrigo Pasquali <rpasquali@teknos.local>
2018-07-18 01:15:44 -0300

    Se realizan test para comprar la lista de inversiones generada por el ManejadorArgumentosEntrada

--- TpFinal/src/test/java/ar/edu/untref/aydoo/TestManejadorArgumentosEntrada.java
+++ TpFinal/src/test/java/ar/edu/untref/aydoo/TestManejadorArgumentosEntrada.java
@@ -1,5 +1,8 @@
 package ar.edu.untref.aydoo;
 
+import java.util.LinkedList;
+import java.util.List;
+
 import org.junit.Assert;
 import org.junit.Test;
 
@@ -15,4 +18,41 @@ public class TestManejadorArgumentosEntrada {
 		
 		Assert.assertEquals(valorEsperado, valorObtenido);
 	}
+	
+	@Test
+	public void laPrimeraInversionDeLaListaDeInversionesDeberiaContenerUnPft() {	  
+		String[] argumentos = {"ind", "pft,90,10,2000", "pfp,90,30,10,4000"};
+		ManejadorArgumentosEntrada manejador = new ManejadorArgumentosEntrada(argumentos);
+		List<String[]> listaInversiones = new LinkedList<String[]>();
+		String[] inversionPft = {"pft","90","10","2000"};		
+		String[] inversionPfp = {"pfp","90","30","10","4000"};
+		listaInversiones.add(inversionPft);
+		listaInversiones.add(inversionPfp);
+		String[] valorEsperado = {"pft", "90", "10", "2000"};		
+		
+		manejador.separarArgumentos();
+		List<String[]> listaObtenida = manejador.getListaInversiones();
+		String[] valorObtenido = listaObtenida.get(0);
+		
+		Assert.assertEquals(valorEsperado, valorObtenido);
+	}
+	
+	@Test
+	public void laSegundaInversionDeLaListaDeInversionesDeberiaContenerUnPfp() {	  
+		String[] argumentos = {"ind", "pft,90,10,2000", "pfp,90,30,10,4000"};
+		ManejadorArgumentosEntrada manejador = new ManejadorArgumentosEntrada(argumentos);
+		List<String[]> listaInversiones = new LinkedList<String[]>();
+		String[] inversionPft = {"pft","90","10","2000"};		
+		String[] inversionPfp = {"pfp","90","30","10","4000"};
+		listaInversiones.add(inversionPft);
+		listaInversiones.add(inversionPfp);
+		String[] valorEsperado = {"pfp","90","30","10","4000"};		
+		
+		manejador.separarArgumentos();
+		List<String[]> listaObtenida = manejador.getListaInversiones();
+		String[] valorObtenido = listaObtenida.get(1);
+		
+		Assert.assertEquals(valorEsperado, valorObtenido);
+	}
+
 }
