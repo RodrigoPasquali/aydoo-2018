c01805d7ee981fc725d3543351fd599028f87d56
Rodrigo Pasquali <rpasquali@teknos.local>
2018-07-18 13:59:33 -0300

    En programa se crea metodo realizarInversiones

--- TpFinal/src/main/java/ar/edu/untref/aydoo/Programa.java
+++ TpFinal/src/main/java/ar/edu/untref/aydoo/Programa.java
@@ -8,14 +8,18 @@ public class Programa {
 		manejador.separarArgumentos();
 		List<String[]> listaInversiones = manejador.getListaInversiones();
 		Inversor inversor = new Inversor();
-		for(int i = 0; i < listaInversiones.size(); i++) {
-			Inversion inversion = new PlazoFijoTradicional(listaInversiones.get(i));
-			inversor.realizarInversion(inversion);
-		}
+		realzarInversiones(listaInversiones, inversor);
 		double gananciaObtenida = inversor.calcularGananciasObtenidas();
 		Impuesto impuesto = new ImpuestosIndividuo(manejador.getTipoCliente());
 		double impuestoObtenido = impuesto.aplicarImpuesto(gananciaObtenida);
 		Salida salida = new Salida(gananciaObtenida, impuestoObtenido);
 		System.out.println(salida.generarSalida());
+	}
+	
+	private static void realzarInversiones(List<String[]> listaInversiones, Inversor inversor) {
+		for(int i = 0; i < listaInversiones.size(); i++) {
+			Inversion inversion = new PlazoFijoTradicional(listaInversiones.get(i));
+			inversor.realizarInversion(inversion);
+		}
 	}
 }
